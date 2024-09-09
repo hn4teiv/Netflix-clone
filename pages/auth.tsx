f9f74ce5df3e479696fb65c5a07f538e576566e5
hn4teiv <22010431@st.phenikaa-uni.edu.vn>
2024-09-09 19:29:57 +0700

    9/9

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -2,11 +2,11 @@ import axios from 'axios';
 import { useState, useCallback } from 'react';
 import Input from "@/components/Input";
 import { signIn } from 'next-auth/react';
-import { useRouter } from 'next/router';
+
 import {FcGoogle} from 'react-icons/fc';
 import {FaGithub} from 'react-icons/fa';
 const Auth = () => {
-  const router = useRouter();
+  
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
@@ -21,14 +21,14 @@ const Auth = () => {
       await signIn('credentials', {
         email,
         password,
-        redirect:false,
-        callbackUrl: '/'
+        
+        callbackUrl: '/profiles'
       });
-      router.push('/');
+      
     } catch (error) {
       console.log(error);
     }
-  }, [email, password,router]);
+  }, [email, password]);
 
   const register = useCallback(async () => {
     try {
@@ -87,7 +87,9 @@ const Auth = () => {
               {variant === 'login' ? 'Login' : 'Sign up'}
             </button>
             <div className="flex flex-row items-center gap-4 mt-8 justify-center">
-                <div className="
+                <div 
+                onClick={()=>signIn('google',{callbackUrl:'/profiles'})}
+                className="
                   w-10
                   h-10
                   bg-white
@@ -104,7 +106,7 @@ const Auth = () => {
                     
                 </div>
                 <div
-                onClick={()=>signIn('github',{callbackUrl:'/'})}
+                onClick={()=>signIn('github',{callbackUrl:'/profiles'})}
                  className="
                   w-10
                   h-10
