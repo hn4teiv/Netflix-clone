151ef966ea77f2d3ca5c2140f84f8977dc62467b
hn4teiv <22010431@st.phenikaa-uni.edu.vn>
2024-09-28 08:17:12 +0700

    home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import useCurrentUser from '@/hooks/useCurrentUser';
-import { signOut } from 'next-auth/react';
+
 import { getSession } from 'next-auth/react';
 import { NextPageContext } from 'next';
+import Navbar from '@/components/Navbar';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -22,16 +22,10 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
-
  
   return (
     <>
-      <h1 className="text-2xl text-green-500">Netflix Clone</h1>
-      <p className="h-10 w-full bg-white">Logged in as: {user?.name}</p>
-      <button className="w-full bg-white" onClick={() => signOut()}>
-        Logout!
-      </button>
+      <Navbar/>
     </>
   );
 }
